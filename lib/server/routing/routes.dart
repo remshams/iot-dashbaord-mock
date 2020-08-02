2b253c1c19dc30372d386c5585a567309145b3a4
Mathias Remshardt <mathias.remshardt@logicline.de>
2020-08-02 13:35:05 +0200

    * updates toPath method for AppRoutes to leverage removeEnumPrefix method

--- lib/server/routing/routes.dart
+++ lib/server/routing/routes.dart
@@ -1,8 +1,8 @@
+import 'package:iot_dashboard_mock/utils/enum.dart';
+
 enum AppRoute { currentAccount, devices, ping }
 
-// TODO Refactor to removePrefix method
-String toPath(dynamic route) =>
-    route.toString().replaceAll('${route.runtimeType}.', '/');
+String toPath(dynamic route) => removeEnumPrefix(route, separator: '/');
 
 extension AppRouteExtension on AppRoute {
   String get path => toPath(this);
